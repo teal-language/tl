fdb0a01ca3caf55bd73bfeade1ad4bc06dbbfaee
Hisham Muhammad <hisham@gobolinux.org>
2019-11-17 22:00:33 -0300

    tests: reorganize error message tests

--- spec/error_reporting/typecheck_error_spec.lua
+++ spec/error_reporting/typecheck_error_spec.lua
@@ -21,42 +21,6 @@ describe("typecheck errors", function()
       assert.is_not_nil(string.match(errors[1].filename, "bar.tl$"), "type errors should contain .filename property")
    end)
 
-   it("syntax errors in a nested required package refer to the correct filename of required file", function ()
-      util.mock_io(finally, {
-         ["aaa.tl"] = [[
-            local bbb = require "bbb"
-
-            function function() end
-
-            local x: string = 1
-         ]],
-         ["bbb.tl"] = [[
-            local bbb = {}
-
-            bbb.y = 2
-
-            if bbb.y end
-
-            return bbb
-         ]],
-         ["foo.tl"] = [[
-            local aaa = require "aaa"
-         ]],
-      })
-      local result, err = tl.process("foo.tl")
-
-      local expected = {
-         { filename = "aaa.tl", y = 3 },
-         { filename = "bbb.tl", y = 5 },
-         { filename = "bbb.tl", y = 7 },
-      }
-      assert.same(#expected, #result.syntax_errors)
-      for i, err in ipairs(result.syntax_errors) do
-         assert.match(expected[i].filename, result.syntax_errors[i].filename, 1, true)
-         assert.same(expected[i].y, result.syntax_errors[i].y)
-      end
-   end)
-
    it("unknowns include filename", function ()
       local tokens = tl.lex("local x: string = b")
       local _, ast = tl.parse_program(tokens, {})
