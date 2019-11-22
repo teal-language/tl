0911390e1d0a69f32cbbd0e60429b2bf2f72dd25
Hisham Muhammad <hisham@gobolinux.org>
2019-11-22 01:01:02 -0300

    add test case for pending bug in unary operators

--- spec/operator/not_spec.lua
+++ spec/operator/not_spec.lua
@@ -1,5 +1,9 @@
 local tl = require("tl")
 
+local function trim_code(c)
+   return c:gsub("^%s*", ""):gsub("\n%s*", "\n"):gsub("%s*$", "")
+end
+
 describe("not", function()
    it("ok with any type", function()
       local tokens = tl.lex([[
@@ -32,4 +36,37 @@ describe("not", function()
       local errors = tl.type_check(ast)
       assert.same({}, errors)
    end)
+   pending("handles precedence of sequential unaries correctly", function()
+      local code = [[
+         local y = not -a == not -b
+         local x = not not a == not not b
+      ]]
+
+      local tokens = tl.lex(code)
+      local _, ast = tl.parse_program(tokens)
+      local output = tl.pretty_print_ast(ast, true)
+
+      assert.same(trim_code(code), trim_code(output))
+   end)
+   pending("handles complex expression with not", function()
+      local code = [[
+         if t1.typevar == t2.typevar and
+            (not not typevars or
+            not not typevars[t1.typevar] == not typevars[t2.typevar]) then
+            return true
+         end
+         if t1.typevar == t2.typevar and
+            (not typevars or
+            not not typevars[t1.typevar] == not not typevars[t2.typevar]) then
+            return true
+         end
+      ]]
+
+      local tokens = tl.lex(code)
+      local _, ast = tl.parse_program(tokens)
+      local output = tl.pretty_print_ast(ast, true)
+
+      assert.same(trim_code(code), trim_code(output))
+   end)
+
 end)
