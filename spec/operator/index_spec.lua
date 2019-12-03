1d94a765750c1da3aeb32659c6ccfa5cb03c6bfc
Hisham Muhammad <hisham@gobolinux.org>
2019-12-03 02:30:53 -0300

    allow indexing records with enums

--- spec/operator/index_spec.lua
+++ spec/operator/index_spec.lua
@@ -32,6 +32,50 @@ describe("[]", function()
          assert.same(1, #errors)
          assert.match("got number, expected string", errors[1].err, 1, true)
       end)
+      it("ok without declaration if key is enum and all keys map to the same type", function()
+         -- pass
+         local tokens = tl.lex([[
+            local Keys = enum
+               "foo"
+               "bar"
+            end
+            local x = { foo = 12, bar = 24, bla = "something else" }
+            local e: Keys = "foo"
+            local n: number = x[e]
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+         -- fail as expected
+         local tokens = tl.lex([[
+            local Keys = enum
+               "foo"
+               "bar"
+            end
+            local x = { foo = 12, bar = true, bla = "something else" }
+            local e: Keys = "foo"
+            local n: number = x[e]
+         ]])
+         _, ast = tl.parse_program(tokens)
+         errors = tl.type_check(ast)
+         assert.same(1, #errors)
+         assert.match("cannot index, not all enum values map to record fields of the same type", errors[1].err, 1, true)
+         -- fail as expected
+         local tokens = tl.lex([[
+            local Keys = enum
+               "foo"
+               "bar"
+               "oops"
+            end
+            local x = { foo = 12, bar = 12, bla = "something else" }
+            local e: Keys = "foo"
+            local n: number = x[e]
+         ]])
+         _, ast = tl.parse_program(tokens)
+         errors = tl.type_check(ast)
+         assert.same(1, #errors)
+         assert.match("enum value 'oops' is not a field", errors[1].err, 1, true)
+      end)
       it("fail if indexing by invalid string", function()
          local tokens = tl.lex([[
             local x = { foo = "f" }
