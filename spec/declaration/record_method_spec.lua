91ca4fecb7793ca9a4712314d94d11c516227e56
Hisham Muhammad <hisham@gobolinux.org>
2020-01-28 00:17:36 -0300

    fix: resolves self but does not output it as an argument (#27)

--- spec/declaration/record_method_spec.lua
+++ spec/declaration/record_method_spec.lua
@@ -1,4 +1,9 @@
 local tl = require("tl")
+local util = require("spec.util")
+
+local function unindent(code)
+   return code:gsub("[ \t]+", " "):gsub("\n[ \t]+", "\n"):gsub("^%s+", ""):gsub("%s+$", "")
+end
 
 describe("record method", function()
    it("valid declaration", function()
@@ -91,6 +96,39 @@ describe("record method", function()
       local _, ast = tl.parse_program(tokens)
       local errors = tl.type_check(ast)
       assert.match("invalid key 'invalid' in record 'self'", errors[1].msg, 1, true)
+   end)
+
+   it("resolves self but does not output it as an argument (#27)", function()
+      util.mock_io(finally, {
+         ["foo.tl"] = [[
+            local r = {
+               x = 2,
+               b = true,
+            }
+            function r:f(a: number, b: string): boolean
+               return self.invalid
+            end
+            function r:g()
+               return
+            end
+            local ok = r:f(3, "abc")
+         ]],
+      })
+      local result, err = tl.process("foo.tl")
+      local output = tl.pretty_print_ast(result.ast)
+      assert.same(unindent[[
+         local r = {
+            ["x"] = 2,
+            ["b"] = true,
+         }
+         function r:f(a, b)
+            return self.invalid
+         end
+         function r:g()
+            return
+         end
+         local ok = r:f(3, "abc")
+      ]], unindent(output))
    end)
 
    it("catches invocation style", function()
