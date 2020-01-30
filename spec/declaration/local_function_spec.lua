e75a73166287a8c044187fa6f007ba355d2699de
Hisham Muhammad <hisham@gobolinux.org>
2020-01-30 00:00:04 -0300

    Accept parameter names inside function declarations
    Fixes #32.
    
    From @pdesaulniers:
    
    >> I think parameter names inside function declarations should be allowed. It
    >> would make writing declaration files a bit easier. Also, text editors would be
    >> able to parse declaration files and show parameter names in tooltips.
    
    to which I replied:
    
    > I agree! I've been annoyed myself by this at times. And also I just realized
    > that the syntax with form `<name>: <type>` in function declarations is needed
    > in varargs, to allow for `...: string` and the like.

--- spec/declaration/local_function_spec.lua
+++ spec/declaration/local_function_spec.lua
@@ -1,6 +1,64 @@
 local tl = require("tl")
 
 describe("local function", function()
+   describe("type", function()
+      it("can have anonymous arguments", function()
+         local tokens = tl.lex([[
+            local f: function(number, string): boolean
+
+            f = function(a: number, b: string): boolean
+               return #b == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("can take names in arguments but names are ignored", function()
+         local tokens = tl.lex([[
+            local f: function(x: number, y: string): boolean
+
+            f = function(a: number, b: string): boolean
+               return #b == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("can take typed vararg in arguments", function()
+         local tokens = tl.lex([[
+            local f: function(x: number, ...: string): boolean
+
+            f = function(a: number, ...: string): boolean
+               return #select(1, ...) == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("cannot take untyped vararg", function()
+         local tokens = tl.lex([[
+            local f: function(number, ...): boolean
+
+            f = function(a: number, ...: string): boolean
+               return #select(1, ...) == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local syntax_errors = {}
+         local _ = tl.parse_program(tokens, syntax_errors)
+         assert.same("cannot have untyped '...' when declaring the type of an argument", syntax_errors[1].msg)
+      end)
+   end)
+
    it("declaration", function()
       local tokens = tl.lex([[
          local function f(a: number, b: string): boolean
