b674ca794b2ebfb21254dd5c7928139cdba0bb3c
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2020-02-01 18:41:53 -0500

    Add tests for module preload

---
+++ spec/preload/preload_spec.lua
@@ -0,0 +1,85 @@
+local tl = require("tl")
+local util = require("spec.util")
+
+describe("preload", function()
+   it("exports global types", function ()
+      -- ok
+      util.mock_io(finally, {
+         ["love.d.tl"] = [[
+            global love_graphics = record
+               print: function(text: string)
+            end
+
+            global love = record
+               graphics: love_graphics
+            end
+         ]],
+         ["foo.tl"] = [[
+            function love.draw()
+               love.graphics.print("<3")
+            end
+         ]],
+      })
+
+      local result, err = tl.process("foo.tl", nil, nil, nil, {"love"})
+
+      assert.same(nil, err)
+      assert.same(0, #result.syntax_errors)
+      assert.same(0, #result.type_errors)
+      assert.same(0, #result.unknowns)
+   end)
+   it("can require multiple modules", function()
+      -- ok
+      util.mock_io(finally, {
+         ["love.d.tl"] = [[
+            global love_graphics = record
+               print: function(text: string)
+            end
+
+            global love = record
+               graphics: love_graphics
+            end
+         ]],
+         ["hate.d.tl"] = [[
+            global hate_graphics = record
+               print: function(text: string)
+            end
+
+            global hate = record
+               graphics: hate_graphics
+            end
+         ]],
+         ["foo.tl"] = [[
+            function love.draw()
+               love.graphics.print("<3")
+            end
+
+            function hate.draw()
+               hate.graphics.print(">:(")
+            end
+         ]],
+      })
+
+      local result, err = tl.process("foo.tl", nil, nil, nil, {"love", "hate"})
+
+      assert.same(nil, err)
+      assert.same(0, #result.syntax_errors)
+      assert.same(0, #result.type_errors)
+      assert.same(0, #result.unknowns)
+   end)
+   it("returns an error when a module doesn't exist", function ()
+      -- ok
+      util.mock_io(finally, {
+         ["foo.tl"] = [[
+            function love.draw()
+               love.graphics.print("<3")
+            end
+         ]],
+      })
+
+      local result, err = tl.process("foo.tl", nil, nil, nil, {"love"})
+
+      assert.same(nil, result)
+      assert.is_not_nil(err)
+    end)
+end)
