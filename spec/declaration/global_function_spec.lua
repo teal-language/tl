22bb20a8c34e99ead923d1d57ed1148f0ba4096a
Hisham Muhammad <hisham@gobolinux.org>
2020-03-22 15:47:07 -0300

    accept both 'function' and 'global function' for global functions
    Since `global` is accepted for variables in the same places where
    `local` is, I suppose `global function` should be accepted as well
    for consistency.
    
    (Eventually the bare `function` syntax could produce a warning
    in .tl files, but for now we accept both forms...)

---
+++ spec/declaration/global_function_spec.lua
@@ -0,0 +1,199 @@
+local tl = require("tl")
+
+describe("global function", function()
+   describe("type", function()
+      it("can have anonymous arguments", function()
+         local tokens = tl.lex([[
+            global f: function(number, string): boolean
+
+            f = function(a: number, b: string): boolean
+               return #b == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("can have type variables", function()
+         local tokens = tl.lex([[
+            global f: function<`a, `b, `c>(`a, `b): `c
+
+            f = function(a: number, b: string): boolean
+               return #b == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("can take names in arguments but names are ignored", function()
+         local tokens = tl.lex([[
+            global f: function(x: number, y: string): boolean
+
+            f = function(a: number, b: string): boolean
+               return #b == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("can take typed vararg in arguments", function()
+         local tokens = tl.lex([[
+            global f: function(x: number, ...: string): boolean
+
+            f = function(a: number, ...: string): boolean
+               return #select(1, ...) == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local _, ast = tl.parse_program(tokens)
+         local errors = tl.type_check(ast)
+         assert.same({}, errors)
+      end)
+
+      it("cannot take untyped vararg", function()
+         local tokens = tl.lex([[
+            global f: function(number, ...): boolean
+
+            f = function(a: number, ...: string): boolean
+               return #select(1, ...) == a
+            end
+            local ok = f(3, "abc")
+         ]])
+         local syntax_errors = {}
+         local _ = tl.parse_program(tokens, syntax_errors)
+         assert.same("cannot have untyped '...' when declaring the type of an argument", syntax_errors[1].msg)
+      end)
+   end)
+
+   for _, decl in ipairs({ "function", "global function" }) do
+      describe("'" .. decl .. "'", function()
+         it("declaration", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f(a: number, b: string): boolean
+                  return #b == a
+               end
+               local ok = f(3, "abc")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same({}, errors)
+         end)
+
+         it("declaration with type variables", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f<`a, `b>(a1: `a, a2: `a, b1: `b, b2: `b): `b
+                  if a1 == a2 then
+                     return b1
+                  else
+                     return b2
+                  end
+               end
+               local ok = f(10, 20, "hello", "world")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same({}, errors)
+         end)
+
+         it("declaration with nil as return", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f(a: number, b: string): nil
+                  return
+               end
+               local ok = f(3, "abc")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same({}, errors)
+         end)
+
+         it("declaration with no return", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f(a: number, b: string): ()
+                  return
+               end
+               f(3, "abc")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same({}, errors)
+         end)
+
+         it("declaration with no return cannot be used in assignment", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f(a: number, b: string): ()
+                  return
+               end
+               local x = f(3, "abc")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same(1, #errors)
+            assert.same("assignment in declaration did not produce an initial value for variable 'x'", errors[1].msg)
+         end)
+
+         it("declaration with return nil can be used in assignment", function()
+            local tokens = tl.lex([[
+               ]] .. decl .. [[ f(a: number, b: string): nil
+                  return
+               end
+               local x = f(3, "abc")
+            ]])
+            local _, ast = tl.parse_program(tokens)
+            local errors = tl.type_check(ast)
+            assert.same(0, #errors)
+         end)
+
+         describe("with function arguments", function()
+            it("has ambiguity without parentheses in function type return", function()
+               local tokens = tl.lex([[
+                  ]] .. decl .. [[ map<`a, `b>(f: function(`a):`b, xs: {`a}): {`b}
+                     local r = {}
+                     for i, x in ipairs(xs) do
+                        r[i] = f(x)
+                     end
+                     return r
+                  end
+                  local function quoted(s: string): string
+                     return "'" .. s .. "'"
+                  end
+
+                  print(table.concat(map(quoted, {"red", "green", "blue"}), ", "))
+               ]])
+               local syntax_errors = {}
+               tl.parse_program(tokens, syntax_errors)
+               assert.same(1, syntax_errors[1].y)
+               assert.same(54 + #decl, syntax_errors[1].x)
+            end)
+
+            it("has no ambiguity with parentheses in function type return", function()
+               local tokens = tl.lex([[
+                  ]] .. decl .. [[ map<`a,`b>(f: function(`a):(`b), xs: {`a}): {`b}
+                     local r = {}
+                     for i, x in ipairs(xs) do
+                        r[i] = f(x)
+                     end
+                     return r
+                  end
+                  local function quoted(s: string): string
+                     return "'" .. s .. "'"
+                  end
+
+                  print(table.concat(map(quoted, {"red", "green", "blue"}), ", "))
+               ]])
+               local _, ast = tl.parse_program(tokens)
+               local errors = tl.type_check(ast)
+               assert.same({}, errors)
+            end)
+         end)
+      end)
+   end
+end)
