7b791de3e2151797e3aebcea1a2b17de3327f7a3
Hisham Muhammad <hisham@gobolinux.org>
2020-04-19 00:24:33 -0300

    correct position when reporting invalid keys

--- spec/operator/dot_spec.lua
+++ spec/operator/dot_spec.lua
@@ -1,26 +1,19 @@
 local tl = require("tl")
+local util = require("spec.util")
 
 describe(".", function()
    describe("on records", function()
-      it("ok", function()
-         local tokens = tl.lex([[
-            local x = { foo = "f" }
-            print(x.foo)
-         ]])
-         local _, ast = tl.parse_program(tokens)
-         local errors = tl.type_check(ast)
-         assert.same({}, errors)
-      end)
-      it("fail", function()
-         local tokens = tl.lex([[
-            local x = { foo = "f" }
-            print(x.bar)
-         ]])
-         local _, ast = tl.parse_program(tokens)
-         local errors = tl.type_check(ast)
-         assert.same(1, #errors)
-         assert.match("invalid key 'bar' in record 'x'", errors[1].msg, 1, true)
-      end)
+      it("ok", util.check [[
+         local x = { foo = "f" }
+         print(x.foo)
+      ]])
+
+      it("fail", util.check_type_error([[
+         local x = { foo = "f" }
+         print(x.bar)
+      ]], {
+         { y = 2, x = 18, msg = "invalid key 'bar' in record 'x'" }
+      }))
    end)
 
    describe("on raw tables", function()
