a03947b6834576bc8b544e2aabe9c8acc78d9856
Hisham Muhammad <hisham@gobolinux.org>
2020-04-18 21:05:56 -0300

    propagate enum type when doing 'or' against an enum string literal

--- spec/operator/or_spec.lua
+++ spec/operator/or_spec.lua
@@ -1,4 +1,5 @@
 local tl = require("tl")
+local util = require("spec.util")
 
 describe("or", function()
    it("map or record matching map", function()
@@ -15,4 +16,33 @@ describe("or", function()
       local errors = tl.type_check(ast)
       assert.same({}, errors)
    end)
+
+   it("string or enum matches enum", function()
+      local tokens = tl.lex([[
+         local Dir = enum
+            "left"
+            "right"
+         end
+
+         local v: Dir = "left"
+         local x: Dir = v or "right"
+         local y: Dir = "right" or v
+      ]])
+      local _, ast = tl.parse_program(tokens)
+      local errors = tl.type_check(ast)
+      assert.same({}, errors)
+   end)
+
+   it("invalid string or enum matches string", util.check_type_error([[
+      local Dir = enum
+         "left"
+         "right"
+      end
+
+      local v: Dir = "left"
+      local x = v or "don't know"
+      v = x
+   ]], {
+      { y = 8, msg = "in assignment: string is not a Dir" }
+   }))
 end)
