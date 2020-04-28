064b9c4efb4c52aab16dcdce11c3acab15fba7c1
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2020-04-27 20:12:34 -0400

    Add option for prepending directories to the module search path (#94)
    * Add option for prepending directories to package.path
    
    * Add test for -I flag
    
    * Make the -I option prepend to package.cpath too
    
    * Add test for package.cpath with -I option

---
+++ spec/cli/include_spec.lua
@@ -0,0 +1,28 @@
+local util = require("spec.util")
+
+describe("-I --include argument", function()
+   it("adds a directory to package.path", function()
+      local name = util.write_tmp_file(finally, "foo.tl", [[
+         require("add")
+         local x: number = add(1, 2)
+
+         assert(x == 3)
+      ]])
+
+      local pd = io.popen("./tl -I spec check " .. name, "r")
+      local output = pd:read("*a")
+      util.assert_popen_close(true, "exit", 0, pd:close())
+      assert.match("0 errors detected", output, 1, true)
+   end)
+   it("adds a directory to package.cpath", function()
+      local name = util.write_tmp_file(finally, "foo.lua", [[
+         print(package.cpath:match("spec/cli/") ~= nil)
+      ]])
+      local pd = io.popen("./tl run -I spec/cli/ " .. name, "r")
+      local output = pd:read("*a")
+      util.assert_popen_close(true, "exit", 0, pd:close())
+      util.assert_line_by_line([[
+         true
+      ]], output)
+   end)
+end)
