8167f3e447899d61ddd3b414e0f7c889e9fe0ef7
Hisham Muhammad <hisham@gobolinux.org>
2020-05-18 20:36:34 -0300

    Fix expansion of type in table literal
    Fixes #111.

--- spec/arguments/array_spec.lua
+++ spec/arguments/array_spec.lua
@@ -14,4 +14,19 @@ describe("array argument", function()
       { y = 6, msg = 'argument 1: got {string | number}, expected {string}' },
       { y = 7, msg = 'argument 1: got number, expected {string}' },
    }))
+
+   it("constructs type of complex array correctly (#111)", util.check_type_error([[
+      local MyRecord = record
+         func: function<K, V>(t: {{K:V}}): {V}
+      end
+
+      local x: {string} = MyRecord.func({
+         {id = 2},
+         {otherkey = "hello"},
+         {id = "yo"},
+      })
+   ]], {
+      { y = 5, "got {number | string}, expected {string}"}
+   }))
+
 end)
