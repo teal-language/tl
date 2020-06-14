b6a918e5283ac1f40308eec30779c71dd13de6dd
Hisham Muhammad <hisham@gobolinux.org>
2020-06-14 00:43:10 -0300

    fix performance regression in tests

--- spec/api/load_spec.lua
+++ spec/api/load_spec.lua
@@ -1,6 +1,14 @@
 describe("tl.load", function()
 
    it("can load Teal code from Lua", function()
+      finally(function()
+         if package.searchers then
+            table.remove(package.searchers, 2)
+         else
+            table.remove(package.loaders, 2)
+         end
+      end)
+
       local lua_code = [[
          -- test.lua
          local tl = require('tl')
