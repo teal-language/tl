b0129ba06d70706361f7104e2f70e28ffbbde341
Hisham Muhammad <hisham@gobolinux.org>
2020-06-14 01:43:34 -0300

    tests: use util.check

--- spec/operator/as_spec.lua
+++ spec/operator/as_spec.lua
@@ -1,50 +1,36 @@
 local tl = require("tl")
+local util = require("spec.util")
 
 describe("cast", function()
-   it("can be used inside table literals", function()
-      local tokens = tl.lex([[
-         local Foo = record
-            x: string
-         end
+   it("can be used inside table literals", util.check [[
+      local Foo = record
+         x: string
+      end
 
-         local bla = {
-            ovo = {} as Foo
-         }
-      ]])
-      local _, ast = tl.parse_program(tokens)
-      local errors = tl.type_check(ast, false, "test.lua")
-      assert.same({}, errors)
-   end)
+      local bla = {
+         ovo = {} as Foo
+      }
+   ]])
 
-   it("can cast to function", function()
-      local tokens = tl.lex([[
-         local x = nil as function()
-      ]])
-      local _, ast = tl.parse_program(tokens)
-      local errors = tl.type_check(ast, false, "test.lua")
-      assert.same({}, errors)
-   end)
+   it("can cast to function", util.check [[
+      local x = nil as function()
+   ]])
 
-   it("can cast to enum", function()
-      local tokens = tl.lex([[
-         local Direction = enum
-            "north"
-            "south"
-            "east"
-            "west"
-         end
+   it("can cast to enum", util.check [[
+      local Direction = enum
+         "north"
+         "south"
+         "east"
+         "west"
+      end
 
-         local function go(d: Direction)
-            print("I am going " .. d .. "!") -- d works as a string!
-         end
+      local function go(d: Direction)
+         print("I am going " .. d .. "!") -- d works as a string!
+      end
 
-         -- a cast can force an invalid value into an enum type
-         go("up" as Direction)
-      ]])
-      local _, ast = tl.parse_program(tokens)
-      local errors = tl.type_check(ast, false, "test.lua")
-      assert.same({}, errors)
-   end)
+      -- a cast can force an invalid value into an enum type
+      go("up" as Direction)
+   ]])
 
    it("can be used inside table literals", function()
       local tokens = tl.lex([[
