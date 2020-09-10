02717f314ac94c4583b6ffe75a498c0f10a7c192
Hisham Muhammad <hisham@gobolinux.org>
2020-09-10 12:15:58 -0300

    Fix type declaration of select()
    Closes #219.

---
+++ spec/stdlib/select_spec.lua
@@ -0,0 +1,19 @@
+local util = require("spec.util")
+
+describe("select", function()
+   it("accepts a number", util.check [[
+      local greeting = select(2, "hi", "hello", 123)
+   ]])
+
+   it("accepts hash", util.check [[
+      local count = select("#", "hi", "hello")
+      print(count / 2)
+   ]])
+
+   it("rejects an invalid first argument", util.check_type_error([[
+      select({}, "hi", "hello")
+   ]], {
+      -- FIXME not ideal message, but it fails on failure cases
+      { msg = "got {}, expected number" },
+   }))
+end)
