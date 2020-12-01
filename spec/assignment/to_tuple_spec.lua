012df21a1b6be96962191af2d208ae75aa2ea1e7
Corey Williamson <euclidianAce@protonmail.com>
2020-11-30 21:28:27 -0600

    Add more tuple tests + cleanup older code

--- spec/assignment/to_tuple_spec.lua
+++ spec/assignment/to_tuple_spec.lua
@@ -6,4 +6,10 @@ describe("assignment to tuple", function()
       local arr = {1, 2, 3, 4}
       t = arr
    ]])
+   it("should error when an array literal is too long", util.check_type_error([[
+      local t: {number, number}
+      t = {1, 2, 3}
+   ]], {
+      { msg = "incompatible length, expected maximum length of 2, got 3", y = 2 },
+   }))
 end)
