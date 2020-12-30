0a80ada5464207075596d40b6f3f5caf8667a73e
Hisham Muhammad <hisham@gobolinux.org>
2020-12-30 16:17:05 -0300

    standard library: fix and improve os.date definition
    Fix the second argument to number.
    
    Fixes #305. Thanks @JLPLabs for the report!
    
    Also, use the enum trick to make a smart return type decision when given the
    special formats `"!*t"` and `"*t"`.

--- spec/stdlib/os_spec.lua
+++ spec/stdlib/os_spec.lua
@@ -7,13 +7,20 @@ describe("os", function()
          print("today is " .. os.date())
       ]])
 
-      it("with arguments returns as string or a table", util.check [[
+      it("can return a table if requested", util.check [[
          local utctable = os.date("!*t")
-         if not utctable is string then
-            print(utctable.year)
-            print(utctable.month)
-            print(utctable.day)
-         end
+         print(utctable.year)
+         print(utctable.month)
+         print(utctable.day)
+
+         local futuretable = os.date("*t", os.time() + 3600)
+         print(futuretable.year)
+         print(futuretable.month)
+         print(futuretable.day)
+      ]])
+
+      it("can return a string if another format is given", util.check [[
+         print("Today is " .. os.date("%A, %F"))
       ]])
    end)
 
