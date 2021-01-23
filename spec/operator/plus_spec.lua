f8a20f1658f0f6d8aaa1e45a4f00c4c77fdc562f
Hisham Muhammad <hisham@gobolinux.org>
2021-01-23 01:48:59 -0300

    fix precedence of +

--- spec/operator/plus_spec.lua
+++ spec/operator/plus_spec.lua
@@ -7,4 +7,11 @@ describe("+", function()
       local z = 3
       z = x + y
    ]])
+
+   it("has correct precedence compared to concatenation", util.check [[
+      local h = "hi"
+      local w = "world"
+
+      print("this has " .. #h + #w .. " characters: ", h, w)
+   ]])
 end)
