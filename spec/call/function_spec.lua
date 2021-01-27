35723b56611bc60648937e2b2ef0a3096cd8c4fd
Hisham Muhammad <hisham@gobolinux.org>
2021-01-27 00:38:02 -0300

    optional arity: enable optional arity checks!

--- spec/call/function_spec.lua
+++ spec/call/function_spec.lua
@@ -32,4 +32,70 @@ describe("function calls", function()
           return foo("hi there", select(2, ...))
       end
    ]]))
+
+   describe("check the arity of functions:", function()
+      it("when excessive", util.check_type_error([[
+         local function f(n: number, m: number): number
+            return n + m
+         end
+
+         local x = f(1, 2, 3)
+      ]], {
+         { y = 5, msg = "wrong number of arguments (given 3, expects 2)" },
+      }))
+
+      it("when insufficient", util.check_type_error([[
+         local function f(n: number, m: number): number
+            return n + m
+         end
+
+         local x = f(1)
+      ]], {
+         { y = 5, msg = "wrong number of arguments (given 1, expects 2)" },
+      }))
+
+      it("when using optional", util.check([[
+         local function f(n: number, m?: number): number
+            return n + (m or 0)
+         end
+
+         local x = f(1)
+      ]]))
+
+      it("when insufficient with optionals", util.check_type_error([[
+         local function f(n: number, m?: number): number
+            return n + (m or 0)
+         end
+
+         local x = f()
+      ]], {
+         { y = 5, msg = "wrong number of arguments (given 0, expects at least 1 and at most 2)" },
+      }))
+
+      it("when using all optionals", util.check([[
+         local function f(n?: number, m?: number): number
+            return (n or 0) + (m or 0)
+         end
+
+         local x = f()
+      ]]))
+
+      it("when using all optionals", util.check([[
+         local function f(n?: number, m?: number): number
+            return (n or 0) + (m or 0)
+         end
+
+         local x = f(1, 2)
+      ]]))
+
+      it("when excessive with optionals", util.check_type_error([[
+         local function f(n: number, m?: number): number
+            return (n or 0) + (m or 0)
+         end
+
+         local x = f(1, 2, 3)
+      ]], {
+         { y = 5, msg = "wrong number of arguments (given 3, expects at least 1 and at most 2)" },
+      }))
+   end)
 end)
