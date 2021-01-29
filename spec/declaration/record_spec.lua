1f3187cd10e23a871968b4a02a0a4bfdf5135071
Hisham Muhammad <hisham@gobolinux.org>
2021-01-29 02:33:13 -0300

    optional arity: add some optionals

--- spec/declaration/record_spec.lua
+++ spec/declaration/record_spec.lua
@@ -133,8 +133,8 @@ for i, name in ipairs({"records", "arrayrecords", "interfaces", "arrayinterfaces
 
       it("can overload functions", util.check([[
          global type love_graphics = ]]..statement..[[ ]]..array(i, "{love_graphics}")..[[
-            print: function(text: string, x: number, y: number, r: number, sx: number, sy: number, ox: number, oy: number, kx: number, ky:number)
-            print: function(coloredtext: {any}, x: number, y: number, r: number, sx: number, sy: number, ox: number, oy: number, kx: number, ky:number)
+            print: function(text: string, x: number, y: number, r?: number, sx?: number, sy?: number, ox?: number, oy?: number, kx?: number, ky?: number)
+            print: function(coloredtext: {any}, x: number, y: number, r?: number, sx?: number, sy?: number, ox?: number, oy?: number, kx?: number, ky?: number)
          end
 
          global type love = ]]..statement..[[ ]]..array(i, "{love}")..[[
