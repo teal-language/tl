ddc4ea0d2649981350fd8b7ff6e1eda6b2804076
Corey Williamson <euclidianAce@protonmail.com>
2021-01-29 20:26:12 -0600

    tests: add test for circular dependency catching

--- spec/cli/build_spec.lua
+++ spec/cli/build_spec.lua
@@ -57,57 +57,79 @@ describe("build command", function()
       end)
    end)
 
-   it("should not recompile when targets are newer than sources", function()
-      util.do_in(util.write_tmp_dir(finally, {
-         ["tlconfig.lua"] = [[
-            return {
-               source_dir = "src",
-               build_dir = "build",
-            }
-         ]],
-         ["src"] = {
-            ["foo.tl"] = [[]]
-         },
-      }), function()
-         assert.are.same(runcmd(build_cmd), "Created directory: build\nWrote: build/foo.lua\n")
-         assert.match(runcmd(build_cmd), "All files up to date\n")
+   describe("lazy rebuilds", function()
+      it("should not recompile when targets are newer than sources", function()
+         util.do_in(util.write_tmp_dir(finally, {
+            ["tlconfig.lua"] = [[
+               return {
+                  source_dir = "src",
+                  build_dir = "build",
+               }
+            ]],
+            ["src"] = {
+               ["foo.tl"] = [[]]
+            },
+         }), function()
+            assert.are.same(runcmd(build_cmd), "Created directory: build\nWrote: build/foo.lua\n")
+            assert.match(runcmd(build_cmd), "All files up to date\n")
+         end)
+      end)
+
+      it("should recompile when sources are newer than targets", function()
+         util.do_in(util.write_tmp_dir(finally, {
+            ["tlconfig.lua"] = [[
+               return {
+                  source_dir = "src",
+                  build_dir = "build",
+               }
+            ]],
+            ["src"] = {
+               ["foo.tl"] = [[]]
+            },
+         }), function()
+            assert.are.same("Created directory: build\nWrote: build/foo.lua\n", runcmd(build_cmd))
+            assert.match("All files up to date\n", runcmd(build_cmd))
+            lfs.touch("src/foo.tl", os.time() + 5)
+            assert.are.same("Wrote: build/foo.lua\n", runcmd(build_cmd))
+         end)
+      end)
+
+      it("should recompile when given --update-all flag", function()
+         util.do_in(util.write_tmp_dir(finally, {
+            ["tlconfig.lua"] = [[
+               return {
+                  source_dir = "src",
+                  build_dir = "build",
+               }
+            ]],
+            ["src"] = {
+               ["foo.tl"] = [[]]
+            },
+         }), function()
+            local cmd = util.tl_cmd("build", "--update-all")
+            assert.are.same("Created directory: build\nWrote: build/foo.lua\n", runcmd(cmd))
+            assert.are.same("Wrote: build/foo.lua\n", runcmd(cmd))
+         end)
       end)
    end)
 
-   it("should recompile when sources are newer than targets", function()
-      util.do_in(util.write_tmp_dir(finally, {
-         ["tlconfig.lua"] = [[
-            return {
-               source_dir = "src",
-               build_dir = "build",
-            }
-         ]],
-         ["src"] = {
-            ["foo.tl"] = [[]]
+   it("should be able to detect circular dependencies", function()
+      util.run_mock_project(finally, {
+         dir_structure = {
+            ["tlconfig.lua"] = [[]],
+            ["foo.tl"] = [[require"bar"]],
+            ["bar.tl"] = [[require"foo"]],
+            ["baz.tl"] = [[require"foo"]],
          },
-      }), function()
-         assert.are.same("Created directory: build\nWrote: build/foo.lua\n", runcmd(build_cmd))
-         assert.match("All files up to date\n", runcmd(build_cmd))
-         lfs.touch("src/foo.tl", os.time() + 5)
-         assert.are.same("Wrote: build/foo.lua\n", runcmd(build_cmd))
-      end)
-   end)
-
-   it("should recompile when given --update-all flag", function()
-      util.do_in(util.write_tmp_dir(finally, {
-         ["tlconfig.lua"] = [[
-            return {
-               source_dir = "src",
-               build_dir = "build",
-            }
-         ]],
-         ["src"] = {
-            ["foo.tl"] = [[]]
+         cmd = "build",
+         popen = {
+            status = nil,
+            exit = "exit",
+            code = 1,
          },
-      }), function()
-         local cmd = util.tl_cmd("build", "--update-all")
-         assert.are.same("Created directory: build\nWrote: build/foo.lua\n", runcmd(cmd))
-         assert.are.same("Wrote: build/foo.lua\n", runcmd(cmd))
-      end)
+         cmd_output = "Circular dependency detected in the following files:\n"
+                   .. "   foo.tl depends on: bar.tl\n"
+                   .. "   bar.tl, baz.tl all depend on: foo.tl\n"
+      })
    end)
 end)
