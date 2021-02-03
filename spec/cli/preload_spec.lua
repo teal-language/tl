b98b7d7a08f16f5ee09e7d322776cca569ab73b4
Hisham Muhammad <hisham@gobolinux.org>
2021-02-02 22:01:49 -0300

    tests: write temp files, don't keep stray .tl files in spec/

--- spec/cli/preload_spec.lua
+++ spec/cli/preload_spec.lua
@@ -3,34 +3,66 @@ local util = require("spec.util")
 
 describe("-l --preload argument", function()
    it("exports globals from a module", function()
-      local name = util.write_tmp_file(finally, [[
-         print(add(10, 20))
-      ]])
+      util.do_in(util.write_tmp_dir(finally, {
+         mod = {
+            ["add.tl"] = [[
+               function add(n: number, m: number): number
+                   return n + m
+               end
 
-      local pd = io.popen(util.tl_cmd("check", "-l", "spec.add", name), "r")
-      local output = pd:read("*a")
-      util.assert_popen_close(true, "exit", 0, pd:close())
-      assert.match("0 errors detected", output, 1, true)
+               return add
+            ]],
+         },
+         ["test.tl"] = [[
+            print(add(10, 20))
+         ]],
+      }), function()
+         local pd = io.popen(util.tl_cmd("check", "-l", "mod.add", "test.tl"), "r")
+         local output = pd:read("*a")
+         util.assert_popen_close(true, "exit", 0, pd:close())
+         assert.match("0 errors detected", output, 1, true)
+      end)
    end)
    it("reports error in stderr and code 1 when a module cannot be found", function ()
-      local name = util.write_tmp_file(finally, [[
-         print(add(10, 20))
-      ]])
+      util.do_in(util.write_tmp_dir(finally, {
+         ["test.tl"] = [[
+             print(add(10, 20))
+         ]],
+      }), function()
+         local pd = io.popen(util.tl_cmd("check", "-l", "module_that_doesnt_exist", "test.tl") .. " 2>&1", "r")
+         local output = pd:read("*a")
+         util.assert_popen_close(nil, "exit", 1, pd:close())
+         assert.match("Error:", output, 1, true)
+      end)
+   end)
+   it("can be used more than once", function ()
+      util.do_in(util.write_tmp_dir(finally, {
+         mod = {
+            ["add.tl"] = [[
+               function add(n: number, m: number): number
+                   return n + m
+               end
 
-      local pd = io.popen(util.tl_cmd("check", "-l", "module_that_doesnt_exist", name) .. " 2>&1 1>/dev/null", "r")
-      local output = pd:read("*a")
-      util.assert_popen_close(nil, "exit", 1, pd:close())
-      assert.match("Error:", output, 1, true)
-    end)
-    it("can be used more than once", function ()
-      local name = util.write_tmp_file(finally, [[
-         print(add(10, 20))
-         print(subtract(20, 10))
-      ]])
+               return add
+            ]],
+            ["subtract.tl"] = [[
+               function subtract(n: number, m: number): number
+                   return n - m
+               end
 
-      local pd = io.popen(util.tl_cmd("check", "-l", "spec.add", "--preload",  "spec.subtract", name), "r")
-      local output = pd:read("*a")
-      util.assert_popen_close(true, "exit", 0, pd:close())
-      assert.match("0 errors detected", output, 1, true)
-    end)
+               return subtract
+            ]],
+         },
+         ["test.tl"] = [[
+             print(add(10, 20))
+             print(subtract(20, 10))
+         ]],
+         src = {},
+      }), function()
+         local pd = io.popen(util.tl_cmd("check", "-l", "mod.subtract", "--preload", "mod.add", "test.tl"), "r")
+         local output = pd:read("*a")
+         util.assert_popen_close(true, "exit", 0, pd:close())
+         assert.match("0 errors detected", output, 1, true)
+      end)
+   end)
 end)
