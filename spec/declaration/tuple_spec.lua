6cc734cbd01c950383ffa12293bd623a58c5d844
Hisham Muhammad <hisham@gobolinux.org>
2021-02-18 21:35:24 -0300

    bidirectional inference for table literals

--- spec/declaration/tuple_spec.lua
+++ spec/declaration/tuple_spec.lua
@@ -13,7 +13,7 @@ describe("tuple declarations", function()
    it("should report when an array literal is too long to fit within tuple type", util.check_type_error([[
       local a: {number, number} = {1, 2, 3}
    ]], {
-      { y = 1, msg = "in local declaration: a: incompatible length, expected maximum length of 2, got 3" }
+      { y = 1, msg = "in local declaration: a: unexpected index 3 in tuple {number, number}" }
    }))
 
    it("should report when an array is of incorrect type of a tuple entry", util.check_type_error([[
@@ -21,9 +21,9 @@ describe("tuple declarations", function()
       local b: {number, string} = { "hi" } -- infers to {string} with a length of 1, which is incompatible with {number, string}
       local c: {string, number} = { [-1] = "hi" } -- infers to {string} with a length of 0, which should maybe be compatible with {string, number}?
    ]], {
-      { y = 1, msg = "in local declaration: a: tuple entry 1 of type number does not match type of array elements, which is string" },
-      { y = 2, msg = "in local declaration: b: tuple entry 1 of type number does not match type of array elements, which is string" },
-      { y = 3, msg = "in local declaration: c: tuple entry 2 of type number does not match type of array elements, which is string" },
+      { y = 1, msg = "in local declaration: a: unknown index in tuple {number, string}" },
+      { y = 2, msg = "in local declaration: b: in tuple: at index 1: got string \"hi\", expected number" },
+      { y = 3, msg = "in local declaration: c: unknown index in tuple {string, number}" },
    }))
 
    it("should allow array literals that fit within then tuple length", util.check [[
@@ -35,13 +35,13 @@ describe("tuple declarations", function()
    it("should report when a tuple has incompatible entries", util.check_type_error([[
       local b: {number, string} = { 1, false }
    ]], {
-      { y = 1, msg = "in local declaration: b: in tuple entry 2: got boolean, expected string" },
+      { y = 1, msg = "in local declaration: b: in tuple: at index 2: got boolean, expected string" },
    }))
 
    it("should report when a tuple literal is longer than annotated type", util.check_type_error([[
      local c: {number, string} = { 1, "hello", 10 }
    ]], {
-      { y = 1, msg = "in local declaration: c: tuple {number, string, number} is too big for tuple {number, string}" },
+      { y = 1, msg = "in local declaration: c: unexpected index 3 in tuple {number, string}" },
    }))
 
    it("should work with explicit integer indices", util.check [[
