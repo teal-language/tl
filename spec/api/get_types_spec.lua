02122835aa18434d14f81838705614b9f88e84dc
Hisham Muhammad <hisham@gobolinux.org>
2022-11-05 10:55:54 -0300

    report resolved type on poly function calls

--- spec/api/get_types_spec.lua
+++ spec/api/get_types_spec.lua
@@ -1,6 +1,6 @@
 local tl = require("tl")
 
-describe("tl.process", function()
+describe("tl.get_types", function()
    it("skips over label nodes (#393)", function()
       local result = assert(tl.process_string([[
          local function a()
@@ -11,5 +11,22 @@ describe("tl.process", function()
       local tr, trenv = tl.get_types(result)
       assert(tr)
       assert(trenv)
+   end)
+
+   it("reports resolved type on poly function calls", function()
+      local result = assert(tl.process_string([[
+         local record R
+            f: function(string)
+            f: function(integer)
+         end
+
+         R.f("hello")
+      ]]))
+
+      local tr, trenv = tl.get_types(result)
+      local y = 6
+      local x = 11
+      local type_at_y_x = tr.by_pos[""][y][x]
+      assert(tr.types[type_at_y_x].str == "function(string)")
    end)
 end)
