20e7b0b7454fc625b233ea3c0bda5c27de5852e5
Hisham Muhammad <hisham@gobolinux.org>
2023-12-10 02:53:15 -0300

    no need to clear tk when uniting flattening constants

--- tl.lua
+++ tl.lua
@@ -8949,9 +8949,7 @@ a.types[i], b.types[i]), }
                new.tk = nil
                table.insert(old.types, new)
             else
-               old.tk = nil
-               new.tk = nil
-               return unite({ old, new })
+               return unite({ old, new }, true)
             end
          end
       end
