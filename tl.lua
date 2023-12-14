33fbb15e33c7d329e4dca34a9751abbf86f9ed5d
Hisham Muhammad <hisham@gobolinux.org>
2023-12-14 03:07:13 -0300

    tl types: produce types for for-in variables

--- tl.lua
+++ tl.lua
@@ -10304,6 +10304,11 @@ a.types[i], b.types[i]), }
                      end
                   end
                   add_var(v, v.tk, r)
+
+                  if store_type then
+                     store_type(v.y, v.x, r)
+                  end
+
                   last = r
                end
                local nrets = #rets.tuple
