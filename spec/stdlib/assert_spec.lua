058baf1f10fcbf333c93f991db409e0b2ff5d586
Hisham Muhammad <hisham@gobolinux.org>
2023-12-29 13:20:39 -0500

    assert function applies its known facts

---
+++ spec/stdlib/assert_spec.lua
@@ -0,0 +1,20 @@
+local util = require("spec.util")
+
+describe("assert", function()
+   it("propagates its known facts", util.check_type_error([[
+      local function f(): string | boolean
+         return true
+      end
+
+      local x: string | boolean
+      local y: string | boolean
+      assert(x is string)
+      print(x .. "!")
+      print(y .. "!")
+      x = f()
+      print(x .. "!")
+   ]], {
+      { y = 9, msg = "cannot use operator '..' for types string | boolean and string" },
+      { y = 11, msg = "cannot use operator '..' for types string | boolean and string" },
+   }))
+end)
