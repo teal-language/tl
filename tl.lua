c54a112e49191a9f08da79516d0c11e57b0b0c4c
Hisham Muhammad <hisham@gobolinux.org>
2023-12-30 13:24:04 -0500

    more efficient nominal check

--- tl.lua
+++ tl.lua
@@ -7867,6 +7867,11 @@ tl.type_check = function(ast, opts)
       },
       ["nominal"] = {
          ["nominal"] = function(a, b)
+            local ok, errs = are_same_nominals(a, b)
+            if ok then
+               return true
+            end
+
             local rb = resolve_nominal(b)
             if rb.typename == "interface" then
 
@@ -7878,12 +7883,6 @@ tl.type_check = function(ast, opts)
 
                return is_a(ra, rb)
             end
-
-            local ok, errs = are_same_nominals(a, b)
-            if ok then
-               return true
-            end
-
 
 
             return ok, errs
