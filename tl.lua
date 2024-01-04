929a606f0467bb549504742ba017e89b5f38a35a
Hisham Muhammad <hisham@gobolinux.org>
2024-01-04 00:39:52 -0300

    fix: define typeargs in function definition for recursion

--- tl.lua
+++ tl.lua
@@ -8548,6 +8548,7 @@ a.types[i], b.types[i]), }
       end
 
       add_var(nil, node.name.tk, a_function({
+         typeargs = node.typeargs,
          args = args,
          rets = get_rets(node.rets),
       }))
