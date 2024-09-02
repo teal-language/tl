d04fa0f6b69c5379952f7ec885366c74f9c36594
Hisham Muhammad <hisham@gobolinux.org>
2024-09-02 11:53:13 -0300

    pragma: ignores other --# lines

--- spec/pragma/invalid_spec.lua
+++ spec/pragma/invalid_spec.lua
@@ -1,17 +1,21 @@
 local util = require("spec.util")
 
 describe("invalid pragma", function()
-   it("rejects invalid pragma", util.check_syntax_error([[
-      --#invalid_pragma on
+   it("ignores other --# lines", util.check([[
+      --#invalid on
+   ]]))
+
+   it("rejects invalid pragma", util.check_type_error([[
+      --#pragma invalid_foo on
    ]], {
-      { y = 1, msg = "invalid token '--#invalid_pragma'" }
+      { y = 1, msg = "invalid pragma: invalid_foo" }
    }))
 
    it("pragmas currently do not accept punctuation", util.check_syntax_error([[
       --#pragma something(other)
    ]], {
-      { y = 1, msg = "invalid token '('" },
-      { y = 1, msg = "invalid token ')'" },
+      { y = 1, x = 26, msg = "invalid token '('" },
+      { y = 1, x = 32, msg = "invalid token ')'" },
    }))
 
    it("pragma arguments need to be in a single line", util.check_syntax_error([[
