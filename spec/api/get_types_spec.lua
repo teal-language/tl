4e42b2fdf3073407e8d23eabb36ae170a06d934c
Hisham Muhammad <hisham@gobolinux.org>
2024-10-07 12:34:55 -0700

    types: reports reference of a nominal type

--- spec/api/get_types_spec.lua
+++ spec/api/get_types_spec.lua
@@ -61,4 +61,37 @@ describe("tl.get_types", function()
       table.sort(fields)
       assert.same(fields, {"init", "x", "y"})
    end)
+
+   it("reports reference of a nominal type", function()
+      local env = tl.init_env()
+      env.report_types = true
+      local result = assert(tl.check_string([[
+         local record Operator
+             operator: string
+         end
+
+         local record Node
+             node1: Node
+             operator: Operator
+         end
+
+         local function node_is_require_call(n: Node): string
+             if n.operator and n.operator.operator == "." then
+                return node_is_require_call(n.node1)
+             end
+         end
+      ]], env))
+
+      local tr, trenv = tl.get_types(result)
+      local y = 7
+      local x = 24
+      local type_at_y_x = tr.by_pos[""][y][x]
+      local ti = tr.types[type_at_y_x]
+      assert(ti)
+      assert.same(ti.str, "Operator")
+      assert(ti.ref)
+      local ti_ref = tr.types[ti.ref]
+      assert(ti ~= ti.ref)
+      assert.same(ti_ref.str, "Operator")
+   end)
 end)
