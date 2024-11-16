4bf78ea0379b6a3e7fae06dcb611f199cb536628
Hisham Muhammad <hisham@gobolinux.org>
2024-11-16 16:50:35 -0300

    fix: error messages for > do not have the operands flipped

---
+++ spec/lang/operator/le_spec.lua
@@ -0,0 +1,41 @@
+local util = require("spec.util")
+
+describe("<=", function()
+   it("ok", util.check([[
+      local x = 1
+      local y = 2
+      local z = true
+      if x <= y then
+         z = false
+      end
+   ]]))
+
+   it("fail", util.check_type_error([[
+      local x = 1
+      local y = "hello"
+      local z = true
+      if x <= y then
+         z = false
+      end
+   ]], {
+      { msg = "cannot use operator '<=' for types integer and string" }
+   }))
+
+   it("fails with not gotcha", util.check_type_error([[
+      local x = 10
+      local y = 20
+      if not x <= y then
+         print("wat")
+      end
+   ]], {
+      { msg = "cannot use operator '<=' for types boolean and integer" }
+   }))
+
+   it("<= on aliases to comparable types return boolean", util.check([[
+      local type Test = integer
+
+      local function f(a: Test, b: Test) : boolean
+         return a <= b
+      end
+   ]]))
+end)
