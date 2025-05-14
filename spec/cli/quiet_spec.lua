6bbff975c876b456c3ce8db9040b6ff7f6c19a91
mbartlett21 <mjmouse9999@gmail.com>
2025-05-15 09:50:59 +1000

    tests: simplify quiet spec

--- spec/cli/quiet_spec.lua
+++ spec/cli/quiet_spec.lua
@@ -1,5 +1,21 @@
 local util = require("spec.util")
 
+local PROGRAM = [[
+local function add(a: number, b: number): number
+   return a + b
+end
+
+print(add(10, 20))
+]]
+
+local COMPILED = [[
+local function add(a, b)
+   return a + b
+end
+
+print(add(10, 20))
+]]
+
 describe("-q --quiet flag", function()
    setup(util.chdir_setup)
    teardown(util.chdir_teardown)
@@ -21,7 +37,7 @@ describe("-q --quiet flag", function()
       local pd = io.popen(util.tl_cmd("check", "-q", name), "r")
       local output = pd:read("*a")
       util.assert_popen_close(0, pd:close())
-      assert.match("", output, 1, true)
+      assert.equal("", output)
    end)
    it("does NOT silence stderr when running tl check", function()
       local name = util.write_tmp_file(finally, [[
@@ -38,25 +54,13 @@ describe("-q --quiet flag", function()
       assert.match("2 errors:", output, 1, true)
    end)
    it("silences stdout when running tl gen", function()
-      local name = util.write_tmp_file(finally, [[
-         local function add(a: number, b: number): number
-            return a + b
-         end
-
-         print(add(10, 20))
-      ]])
+      local name = util.write_tmp_file(finally, PROGRAM)
       local pd = io.popen(util.tl_cmd("gen", "--quiet", name), "r")
       local output = pd:read("*a")
       util.assert_popen_close(0, pd:close())
       local lua_name = name:gsub("tl$", "lua")
-      assert.match("", output, 1, true)
-      util.assert_line_by_line([[
-         local function add(a, b)
-            return a + b
-         end
-
-         print(add(10, 20))
-      ]], util.read_file(lua_name))
+      assert.equal("", output)
+      util.assert_line_by_line(COMPILED, util.read_file(lua_name))
    end)
    it("does NOT silence stderr when running tl gen", function()
       local name = util.write_tmp_file(finally, [[
