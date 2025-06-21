5b460c6d63e34d36a8c694a94df645bcba5f61dd
Hisham Muhammad <hisham@gobolinux.org>
2025-06-21 20:59:19 -0300

    split teal.environment module

---
+++ teal/environment.lua
@@ -0,0 +1,63 @@
+local lua_generator = require("teal.gen.lua_generator")
+
+local type_reporter = require("teal.type_reporter")
+
+
+local variables = require("teal.variables")
+
+
+local types = require("teal.types")
+
+
+local parser = require("teal.parser")
+
+
+local errors = require("teal.errors")
+
+
+local environment = { CheckOptions = {}, Env = {}, Result = {} }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return environment
