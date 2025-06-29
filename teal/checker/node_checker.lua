3a3f0d5591382dfcc2ea28c3e14ff740c18731f4
Hisham Muhammad <hisham@gobolinux.org>
2025-06-29 20:26:57 -0300

    refactor: major refactor of the compiler backend

---
+++ teal/checker/node_checker.lua
@@ -0,0 +1,33 @@
+local environment = require("teal.environment")
+
+
+local facts = require("teal.facts")
+
+
+
+
+
+local types = require("teal.types")
+
+
+
+
+
+
+local type_checker = require("teal.checker.type_checker")
+
+
+local node_checker = {}
+
+
+
+
+
+
+
+
+
+
+
+
+return node_checker
