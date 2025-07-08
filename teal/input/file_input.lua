985b0a0861f4cc9f31f7ef21dce2d1ba565da44c
Hisham Muhammad <hisham@gobolinux.org>
2025-07-08 20:07:48 -0300

    refactor: make package_loader use input.file_input

--- teal/input/file_input.lua
+++ teal/input/file_input.lua
@@ -1,14 +1,24 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local io = _tl_compat and _tl_compat.io or io; local string_input = require("teal.input.string_input")
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local io = _tl_compat and _tl_compat.io or io; local string = _tl_compat and _tl_compat.string or string; local string_input = require("teal.input.string_input")
 
 
 
 
-
-local util = require("teal.util")
-local read_file_skipping_bom = util.read_file_skipping_bom
 
 local file_input = {}
 
+
+local function read_file_skipping_bom(fd)
+   local bom = "\239\187\191"
+   local content, err = fd:read("*a")
+   if not content then
+      return nil, err
+   end
+
+   if content:sub(1, bom:len()) == bom then
+      content = content:sub(bom:len() + 1)
+   end
+   return content, err
+end
 
 function file_input.check(env, filename, fd)
    if env.loaded and env.loaded[filename] then
