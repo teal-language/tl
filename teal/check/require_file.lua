41ee4ae8980b3e9ba5aba2d9912b3cdee2c6bbaa
Hisham Muhammad <hisham@gobolinux.org>
2025-07-08 00:20:49 -0300

    refactor: move string and file handlers to teal.input

--- teal/check/require_file.lua
+++ teal/check/require_file.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local io = _tl_compat and _tl_compat.io or io; local os = _tl_compat and _tl_compat.os or os; local package = _tl_compat and _tl_compat.package or package; local string = _tl_compat and _tl_compat.string or string; local table = _tl_compat and _tl_compat.table or table; local file_checker = require("teal.check.file_checker")
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert; local io = _tl_compat and _tl_compat.io or io; local os = _tl_compat and _tl_compat.os or os; local package = _tl_compat and _tl_compat.package or package; local string = _tl_compat and _tl_compat.string or string; local table = _tl_compat and _tl_compat.table or table; local file_input = require("teal.input.file_input")
 
 
 
@@ -102,7 +102,7 @@ function require_file.require_module(env, w, module_name)
    env.module_filenames[module_name] = found
    env.modules[module_name] = a_circular_require(w)
 
-   local found_result, err = file_checker.check(env, found, fd)
+   local found_result, err = file_input.check(env, found, fd)
    assert(found_result, err)
 
    env.modules[module_name] = found_result.type
