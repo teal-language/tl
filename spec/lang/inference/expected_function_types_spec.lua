95685db77cf5b10bad8694a750bc749b204bcf31
nikitabaghela <nikitabaghela903@gmail.com>
2026-02-21 21:26:55 +0530

    Refactor test descriptions for clarity

--- spec/lang/inference/expected_function_types_spec.lua
+++ spec/lang/inference/expected_function_types_spec.lua
@@ -1,7 +1,8 @@
 local util = require("spec.util")
 
-describe("expected function type propagation", function()
+describe("function definition type propagation", function()
 
+   -- Argument type propagation
    it("propagates expected type in variable assignment", util.check([[
       local f: function(integer): string = function(x)
          local added: integer = x + 1
@@ -9,6 +10,7 @@ describe("expected function type propagation", function()
       end
    ]]))
 
+   -- Argument type propagation in function calls
    it("propagates expected type in function argument", util.check([[
       local function call_with_function(f: function(integer): string): string
          return f(42)
@@ -19,6 +21,8 @@ describe("expected function type propagation", function()
          return tostring(added)
       end)
    ]]))
+
+   -- Return type propagation should not apply in assignment context
    it("does not propagate expected returns in assignment context", util.check_type_error([[
       local f: function(integer): string
       f = function(x)
@@ -26,9 +30,12 @@ describe("expected function type propagation", function()
          return tostring(added)
       end
    ]], {
-      { y = 2, msg = "in assignment: incompatible...", },
-      { y = 3, msg = "excess return values...", },
+      { y = 2, msg = "in assignment: incompatible number of returns: got 0 (), expected 1 (string)" },
+      { y = 3, msg = "cannot use operator '+' for types <any type> and integer" },
+      { y = 4, msg = "in return value: excess return values, expected 0 (), got 1 (string)" },
    }))
+
+   -- Return type propagation in return position
    it("propagates expected type in return position", util.check([[
       local function get_handler(): function(integer): string
          return function(x: integer): string
@@ -39,3 +46,4 @@ describe("expected function type propagation", function()
    ]]))
 
 end)
+
