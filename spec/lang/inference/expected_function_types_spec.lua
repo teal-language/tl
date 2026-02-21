2bfd87d869fe86efa5f2fcbe6d9bb29d17cddcfc
nikitabaghela <nikitabaghela903@gmail.com>
2026-02-21 20:09:14 +0530

    Update return statements to use 'added' variable

--- spec/lang/inference/expected_function_types_spec.lua
+++ spec/lang/inference/expected_function_types_spec.lua
@@ -5,7 +5,7 @@ describe("expected function type propagation", function()
    it("propagates expected type in variable assignment", util.check([[
       local f: function(integer): string = function(x)
          local added: integer = x + 1
-         return tostring(x)
+         return tostring(added)
       end
    ]]))
 
@@ -16,14 +16,14 @@ describe("expected function type propagation", function()
 
       call_with_function(function(x)
          local added: integer = x + 1
-         return tostring(x)
+         return tostring(added)
       end)
    ]]))
    it("does not propagate expected returns in assignment context", util.check_type_error([[
       local f: function(integer): string
       f = function(x)
          local added: integer = x + 1
-         return tostring(x)
+         return tostring(added)
       end
    ]], {
       { y = 2, msg = "in assignment: incompatible...", },
@@ -33,7 +33,7 @@ describe("expected function type propagation", function()
       local function get_handler(): function(integer): string
          return function(x: integer): string
             local added: integer = x + 1
-            return tostring(x)
+            return tostring(added)
          end
       end
    ]]))
